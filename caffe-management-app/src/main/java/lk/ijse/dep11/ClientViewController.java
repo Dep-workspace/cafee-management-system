4505f41aac65310d07ecfd7ad6df638e330cb561
PrasadPrabash <prasad.lakman@gmail.com>
2023-09-25 18:39:36 +0530

    Client app GUI complete

---
+++ caffe-management-app/src/main/java/lk/ijse/dep11/ClientViewController.java
@@ -0,0 +1,32 @@
+package lk.ijse.dep11;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ClientViewController {
+    public AnchorPane rootOrder;
+    public TextField txtName;
+    public TextField txtId;
+    public TextField txtContact;
+    public Button btnNewOrder;
+    public TableView tblOrderDetails;
+    public Button btnPlaceOrder;
+    public Spinner<Integer> spnBigKing;
+    public Spinner<Integer> spnSupremeChicken;
+    public Spinner<Integer> spnTexasSmoke;
+    public Spinner spnWhopperBeef;
+
+    public void initialize(){
+
+    }
+
+    public void btnNewOrderOnAction(){
+
+    }
+    public void btnPlaceOrderOnAction(){
+
+    }
+}
